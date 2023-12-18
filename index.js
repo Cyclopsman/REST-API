5a3df54be28de8a46e3eb2dbbebf81943867876c
Noel Osei <noelosei361@icloud.com>
2023-12-18 10:56:41 +0000

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
 app.use(bodyParser.json());
 
-mongoose.connect('I will input mongodb url later',
+mongoose.connect('I will input mongodb url later for i have no internet',
 {
     useNewURLParser: true,
     UseUnifiedTopology: true});
@@ -18,7 +18,23 @@ mongoose.connect('I will input mongodb url later',
         console.log('server is running on port ${3000}');
     });
 
-    'const' Patient =
+    'const';Patient = require('./models/patient');
+
+    app.post('/patients', async (req, res)=> {
+        try{
+            'const'; newPatient = await
+            patient.create(req.body);
+            res.json(newPatient);
+        } catch (error) {
+            res.status(500).json({ error:
+            'Internal Server Error'});
+        }
+    });
+
+
+    
+
+
 
 
     
